d3c98cab0f60e34862a68c1f8fe8075cebab7c50
asep rudi <aseprudi.luqmanul@gmail.com>
2021-09-05 14:51:56 +0700

    responsive section1

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ new fullScroll({
 });
 
 feather.replace()
+
+$('.hamburgermenu').on('click', function(){
+    $('div.hamburgermenu span:nth-child(1)').toggleClass('rotateTop');
+    $('div.hamburgermenu span:nth-child(3)').toggleClass('rotateBottom');
+    $('div.hamburgermenu span:nth-child(2)').toggleClass('hide');
+    $('ul.list').toggleClass('slide');
+
+
+})
